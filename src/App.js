54da0d438ab18fb5f75117d02f62b0b77c6332a2
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-16 14:40:26 +0700

    isntall aos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Router from "./Route";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
   return (
     <div className="App">
       <Router />
