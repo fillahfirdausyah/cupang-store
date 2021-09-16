12ab51e740e1e44df2c2ad5a5ae6ee3e24a75b33
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-16 14:01:15 +0700

    make dummy data product

---
+++ src/dataProduct.js
@@ -0,0 +1,41 @@
+import Product1 from "./Assets/img/product1.jpg";
+import Product2 from "./Assets/img/product2.jpg";
+import Product3 from "./Assets/img/product3.jpg";
+import Product4 from "./Assets/img/product4.jpg";
+import Product5 from "./Assets/img/product5.jpg";
+import Product6 from "./Assets/img/product6.jpg";
+
+const dataProduct = [
+  {
+    id: 1,
+    name: "Sling Pouch TINDER",
+    img: Product1,
+  },
+  {
+    id: 2,
+    name: "Hand Bag WIEDER",
+    img: Product2,
+  },
+  {
+    id: 3,
+    name: "Sling Bag GRUNDER",
+    img: Product3,
+  },
+  {
+    id: 4,
+    name: "T Shirt VIODER",
+    img: Product4,
+  },
+  {
+    id: 5,
+    name: "T Shirt SNODER",
+    img: Product5,
+  },
+  {
+    id: 6,
+    name: "T Shirt DRAGDER",
+    img: Product6,
+  },
+];
+
+export default dataProduct;
