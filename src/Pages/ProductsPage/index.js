bd5dc134b7becd81b404b3b212740a913198d831
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-17 10:28:25 +0700

    add content to product page

--- src/Pages/ProductsPage/index.js
+++ src/Pages/ProductsPage/index.js
@@ -1,10 +1,32 @@
 import React from "react";
+
+import { Navbar, Footer, ProductCard } from "../../Component";
+import dataProduct from "../../dataProduct";
+
 import "./style.css";
 
 function Productpage() {
   return (
     <div className="__productPage">
-      <h1>Product Page</h1>
+      <Navbar />
+      <div className="container">
+        {/* Main */}
+        <div className="__product">
+          <div className="row">
+            {dataProduct.map((data) => (
+              <div className="col-lg-3" key={data.id}>
+                <ProductCard
+                  img={data.img}
+                  name={data.name}
+                  bestSeller={data.bestSeller}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* End Main */}
+      </div>
+      <Footer />
     </div>
   );
 }
