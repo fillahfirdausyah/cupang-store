03434a11d0f05a29ad9564c648a082b4d899e495
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-17 10:30:03 +0700

    add best seller to data dummy

--- src/dataProduct.js
+++ src/dataProduct.js
@@ -10,31 +10,37 @@ const dataProduct = [
     id: 1,
     name: "Sling Pouch TINDER",
     img: Product1,
+    bestSeller: false,
   },
   {
     id: 2,
     name: "Hand Bag WIEDER",
     img: Product2,
+    bestSeller: true,
   },
   {
     id: 3,
     name: "Sling Bag GRUNDER",
     img: Product3,
+    bestSeller: false,
   },
   {
     id: 4,
     name: "T Shirt VIODER",
     img: Product4,
+    bestSeller: true,
   },
   {
     id: 5,
     name: "T Shirt SNODER",
     img: Product5,
+    bestSeller: false,
   },
   {
     id: 6,
     name: "T Shirt DRAGDER",
     img: Product6,
+    bestSeller: false,
   },
 ];
 
