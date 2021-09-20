f223d4f545bd74c2fe47e07823c994d7c878c934
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-20 15:42:53 +0700

    make product category section

--- src/Pages/ProductsPage/index.js
+++ src/Pages/ProductsPage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Navbar, Footer, ProductCard } from "../../Component";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import dataProduct from "../../dataProduct";
 
 import "./style.css";
@@ -12,9 +13,9 @@ function Productpage() {
       <div className="container">
         {/* Main */}
         <div className="__product">
-          <div className="row">
+          <div className="row col-product">
             {dataProduct.map((data) => (
-              <div className="col-lg-3" key={data.id}>
+              <div className="col-lg-4 col-md-6 col-sm-6 col-12" key={data.id}>
                 <ProductCard
                   img={data.img}
                   name={data.name}
@@ -22,6 +23,25 @@ function Productpage() {
                 />
               </div>
             ))}
+          </div>
+          <div className="__productCategory">
+            <h1>Categories</h1>
+            <div className="__listProductCategory">
+              <ul>
+                <li>
+                  <ArrowForwardIosIcon className="arrowIcon" />
+                  Tas
+                </li>
+                <li>
+                  <ArrowForwardIosIcon className="arrowIcon" />
+                  Shirt
+                </li>
+                <li>
+                  <ArrowForwardIosIcon className="arrowIcon" />
+                  Jacket
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         {/* End Main */}
