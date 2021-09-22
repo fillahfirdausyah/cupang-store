dfb0e6dc4e69b24fff6f7e1385c1fdceeaeb1c2b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-22 10:55:38 +0700

    add content to dashboard page

--- src/Pages/DashboardPage/index.js
+++ src/Pages/DashboardPage/index.js
@@ -1,9 +1,55 @@
 import React from "react";
 
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import CategoryIcon from "@material-ui/icons/Category";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import LocalMallIcon from "@material-ui/icons/LocalMall";
+
+import "./style.css";
+
 function DashboardPage() {
   return (
     <div className="__dashboardPage">
-      <h1>Dashboard Page</h1>
+      <div className="__dashboardHeader">Dashboard</div>
+      <div className="__sideNav">
+        <div className="container">
+          <ul>
+            <li>
+              <DashboardIcon />
+            </li>
+            <li>
+              <AddBoxIcon />
+            </li>
+            <li>
+              <CategoryIcon />
+            </li>
+            <li>
+              <ExitToAppIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="__mainDashboard">
+        <div className="__cardInfo __cardInfo-primary">
+          <div className="__cardInfoLeft">
+            <h4>Total Product</h4>
+            <span>1</span>
+          </div>
+          <div className="__cardInfoRight">
+            <LocalMallIcon />
+          </div>
+        </div>
+        <div className="__cardInfo __cardInfo-secondary">
+          <div className="__cardInfoLeft">
+            <h4>Total Category</h4>
+            <span>5</span>
+          </div>
+          <div className="__cardInfoRight">
+            <CategoryIcon />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
