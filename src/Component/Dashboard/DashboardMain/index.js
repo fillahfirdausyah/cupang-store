e3064b7039b7db44ecfa18c26cfb45d74554dfe4
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-22 12:19:11 +0700

    add table to dashboard main component

--- src/Component/Dashboard/DashboardMain/index.js
+++ src/Component/Dashboard/DashboardMain/index.js
@@ -26,6 +26,39 @@ function DashboardMain() {
           <CategoryIcon />
         </div>
       </div>
+      <div className="__listProductDashboard">
+        <h1>List Product</h1>
+        <table class="table table-radius table-dark">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Category</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry the Bird</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
