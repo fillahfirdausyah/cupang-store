8188a42d2ba347cfd44256997d38223efd50df80
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-22 12:35:28 +0700

    render the dashboard header name dynamic

--- src/Component/Dashboard/DashboardHeader/index.js
+++ src/Component/Dashboard/DashboardHeader/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 import "./style.css";
 
 function DashboardHeader() {
-  return <div className="__dashboardHeader">Dashboard</div>;
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+  return (
+    <div className="__dashboardHeader">
+      {`${
+        splitLocation[1].charAt(0).toUpperCase() + splitLocation[1].slice(1)
+      } `}
+      {splitLocation[2] === undefined
+        ? ""
+        : splitLocation[2].charAt(0).toUpperCase() + splitLocation[2].slice(1)}
+    </div>
+  );
 }
 
 export default DashboardHeader;
