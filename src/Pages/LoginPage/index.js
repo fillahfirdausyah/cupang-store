263df725d07becb9ae354250ab4f0daf48f3ac65
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-22 09:01:14 +0700

    make login page component

---
+++ src/Pages/LoginPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+function LoginPage() {
+  return <div className="__loginPage"></div>;
+}
+
+export default LoginPage;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,5 +4,14 @@ import FaqPage from "./FaqPage";
 import HomePage from "./HomePage";
 import HowToOrderPage from "./HowToOrderPage";
 import ProductPage from "./ProductsPage";
+import LoginPage from "./LoginPage";
 
-export { AboutPage, BlogPage, FaqPage, HomePage, HowToOrderPage, ProductPage };
+export {
+  AboutPage,
+  BlogPage,
+  FaqPage,
+  HomePage,
+  HowToOrderPage,
+  ProductPage,
+  LoginPage,
+};
