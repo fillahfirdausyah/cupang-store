dde95ae6f878db51a474c9f9607c35a211b7b922
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-23 11:31:52 +0700

    finist the CRD action for Category

--- src/Pages/Admin/ListCategoryPage/index.js
+++ src/Pages/Admin/ListCategoryPage/index.js
@@ -1,11 +1,54 @@
-import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Toast, ToastContainer } from "react-bootstrap";
 
+import api from "../../../Helpers/api-endpoint";
 import { DashboardNav, DashboardHeader } from "../../../Component";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import "./style.css";
 
 function ListCategoryPage() {
   const [showModal, setShowModal] = useState(false);
+  const [showToastNotify, setShowToastNotify] = useState(false);
+  const [reload, setReload] = useState(false);
+
+  const [categoryName, setCategoryName] = useState("");
+  const [categoriesData, setCategoriesData] = useState([]);
+  const [resMessage, setResMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let categories = await api.get("/api/category");
+      setCategoriesData(categories.data);
+    };
+
+    fetchData();
+  }, [reload]);
+
+  // Add category
+  const addCategory = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/category", {
+        category: categoryName,
+      })
+      .then((res) => setResMessage(res.data.success))
+      .catch((err) => console.log(err));
+    setCategoryName("");
+    setShowModal(false);
+    setShowToastNotify(true);
+    setReload(!reload);
+  };
+
+  // Delete Category
+  const deleteCategory = (id) => {
+    api
+      .delete(`/api/category/${id}`)
+      .then((res) => setResMessage(res.data.success))
+      .catch((err) => console.log(err));
+    setShowToastNotify(true);
+    setReload(!reload);
+  };
+
   return (
     <div className="__dashboardPage">
       <DashboardHeader />
@@ -29,29 +72,28 @@ function ListCategoryPage() {
                   <tr>
                     <th scope="col">No</th>
                     <th scope="col">Category</th>
-                    <th scope="col">Jumlah Product</th>
-                    <th scope="col">Aksi</th>
+                    {/* <th scope="col">Jumlah Product</th> */}
+                    <th scope="col" style={{ textAlign: "center" }}>
+                      Aksi
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>hallo</td>
-                    <td>hallo</td>
-                    <td>@twitter</td>
-                  </tr>
+                  {categoriesData.map((x, index) => (
+                    <tr key={x.id}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{x.category}</td>
+                      {/* <td>Otto</td> */}
+                      <td className="__actionSection">
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => deleteCategory(x.id)}
+                        >
+                          <DeleteForeverIcon /> Hapus
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
@@ -59,6 +101,15 @@ function ListCategoryPage() {
             <ModalAddCategory
               show={showModal}
               onHide={() => setShowModal(false)}
+              categoryName={categoryName}
+              setCategoryName={setCategoryName}
+              addCategory={addCategory}
+            />
+
+            <MyToast
+              showToastNotify={showToastNotify}
+              setShowToastNotify={setShowToastNotify}
+              resMessage={resMessage}
             />
           </div>
         </div>
@@ -67,22 +118,42 @@ function ListCategoryPage() {
   );
 }
 
+function MyToast(props) {
+  return (
+    <ToastContainer position="top-end" className="toast_notify">
+      <Toast
+        show={props.showToastNotify}
+        onClose={() => props.setShowToastNotify(false)}
+        bg="success"
+        delay={2000}
+        autohide
+      >
+        <Toast.Header>
+          <strong className="me-auto">Notifikasi</strong>
+        </Toast.Header>
+        <Toast.Body className="text-white">{props.resMessage}</Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+}
+
 function ModalAddCategory(props) {
   return (
     <Modal
-      {...props}
+      show={props.show}
+      onHide={props.onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
       backdrop="static"
     >
-      <Modal.Header className="__modalAddProductHeadFoot">
+      <Modal.Header className="__modalAddProductHeadFoot" closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Tambah Category
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className="__modalAddProduct">
-        <form>
+        <form onSubmit={props.addCategory}>
           <div className="mb-3">
             <label htmlFor="category" className="form-label">
               Nama Category
@@ -93,6 +164,9 @@ function ModalAddCategory(props) {
               id="category"
               placeholder="Nama Category"
               required
+              autoComplete="off"
+              value={props.categoryName}
+              onChange={(e) => props.setCategoryName(e.target.value)}
             />
           </div>
           <button className="btn btn-primary w-100">Tambah</button>
