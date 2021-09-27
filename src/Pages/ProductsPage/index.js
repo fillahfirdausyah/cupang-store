313a40cd2865932f7296e783504671c03ca54f96
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-27 11:21:56 +0700

    make search product function

--- src/Pages/ProductsPage/index.js
+++ src/Pages/ProductsPage/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
 import api from "../../Helpers/api-endpoint";
 
 import { Navbar, Footer, ProductCard } from "../../Component";
@@ -11,6 +12,8 @@ function Productpage() {
   const [product, setProduct] = useState([]);
   const [category, setCategory] = useState([]);
   const [stateNumber, setStateNumber] = useState(5);
+  const [searchProduct, setSearchProduct] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -31,6 +34,22 @@ function Productpage() {
 
     fetchCategory();
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchProduct.length > 0) {
+        setIsLoading(true);
+        let theData = await api.post("/api/search", {
+          title: searchProduct,
+        });
+        setProduct(theData.data);
+        setIsLoading(false);
+      } else {
+        api.get("/api/product").then((res) => setProduct(res.data));
+      }
+    };
+    fetchData();
+  }, [searchProduct]);
 
   const filterByCategory = async (category) => {
     try {
@@ -54,27 +73,47 @@ function Productpage() {
       <div className="container">
         {/* Main */}
         <div className="__searchProductWrapper">
-          <input autoComplete="off" type="text" placeholder="Cari Product.." />
+          <input
+            autoComplete="off"
+            type="text"
+            placeholder="Cari Product.."
+            value={searchProduct}
+            onChange={(e) => setSearchProduct(e.target.value)}
+          />
           <SearchIcon />
         </div>
         <div className="__product">
-          <div className="row col-product">
-            {product.map((data) => (
-              <div className="col-lg-4 col-md-6 col-sm-6 col-12" key={data.id}>
-                <ProductCard
-                  img={data.image}
-                  name={data.title}
-                  // bestSeller={data.bestSeller}
-                />
-              </div>
-            ))}
-            <button
-              className="btn btn-primary load_more_lg"
-              onClick={() => setStateNumber(stateNumber + 2)}
-            >
-              Load More
-            </button>
-          </div>
+          {isLoading ? (
+            <div className="__loadingSpinnerProduct">
+              <Spinner
+                animation="border"
+                className="custom"
+                size="lg"
+                variant="primary"
+              />
+            </div>
+          ) : (
+            <div className="row col-product">
+              {product.map((data) => (
+                <div
+                  className="col-lg-4 col-md-6 col-sm-6 col-12"
+                  key={data.id}
+                >
+                  <ProductCard
+                    img={data.image}
+                    name={data.title}
+                    // bestSeller={data.bestSeller}
+                  />
+                </div>
+              ))}
+              <button
+                className="btn btn-primary load_more_lg"
+                onClick={() => setStateNumber(stateNumber + 2)}
+              >
+                Load More
+              </button>
+            </div>
+          )}
           <button
             className="btn btn-primary load_more_sm"
             onClick={() => setStateNumber(stateNumber + 2)}
