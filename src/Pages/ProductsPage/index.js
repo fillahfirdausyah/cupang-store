6b85c9b2ac3e4507d4e95408b40d89ab1a9ac7e2
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-27 10:33:50 +0700

    display the product and make function to filter product by category

--- src/Pages/ProductsPage/index.js
+++ src/Pages/ProductsPage/index.js
@@ -1,45 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../Helpers/api-endpoint";
 
 import { Navbar, Footer, ProductCard } from "../../Component";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import dataProduct from "../../dataProduct";
+import SearchIcon from "@material-ui/icons/Search";
 
 import "./style.css";
 
 function Productpage() {
+  const [product, setProduct] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [stateNumber, setStateNumber] = useState(5);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      let products = await api.post("/api/search", {
+        amount: stateNumber,
+      });
+      setProduct(products.data);
+    };
+
+    fetchProduct();
+  }, [stateNumber]);
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      let categories = await api.get("/api/category");
+      setCategory(categories.data);
+    };
+
+    fetchCategory();
+  }, []);
+
+  const filterByCategory = async (category) => {
+    try {
+      if (category === "semua") {
+        let theData = await api.get("/api/product");
+        setProduct(theData.data);
+        window.scrollTo(0, 0);
+      } else {
+        let theData = await api.post("/api/search", {
+          category,
+        });
+        setProduct(theData.data);
+        window.scrollTo(0, 0);
+      }
+    } catch (err) {}
+  };
+
   return (
     <div className="__productPage">
       <Navbar />
       <div className="container">
         {/* Main */}
+        <div className="__searchProductWrapper">
+          <input autoComplete="off" type="text" placeholder="Cari Product.." />
+          <SearchIcon />
+        </div>
         <div className="__product">
           <div className="row col-product">
-            {dataProduct.map((data) => (
+            {product.map((data) => (
               <div className="col-lg-4 col-md-6 col-sm-6 col-12" key={data.id}>
                 <ProductCard
-                  img={data.img}
-                  name={data.name}
-                  bestSeller={data.bestSeller}
+                  img={data.image}
+                  name={data.title}
+                  // bestSeller={data.bestSeller}
                 />
               </div>
             ))}
+            <button
+              className="btn btn-primary load_more_lg"
+              onClick={() => setStateNumber(stateNumber + 2)}
+            >
+              Load More
+            </button>
           </div>
+          <button
+            className="btn btn-primary load_more_sm"
+            onClick={() => setStateNumber(stateNumber + 2)}
+          >
+            Load More
+          </button>
+
           <div className="__productCategory">
             <h1>Categories</h1>
             <div className="__listProductCategory">
               <ul>
-                <li>
-                  <ArrowForwardIosIcon className="arrowIcon" />
-                  Tas
+                <li onClick={() => filterByCategory("semua")}>
+                  <ArrowForwardIosIcon className="arrowIcon" /> Semua
                 </li>
-                <li>
-                  <ArrowForwardIosIcon className="arrowIcon" />
-                  Shirt
-                </li>
-                <li>
-                  <ArrowForwardIosIcon className="arrowIcon" />
-                  Jacket
-                </li>
+                {category.map((x) => (
+                  <li key={x.id} onClick={() => filterByCategory(x.category)}>
+                    <ArrowForwardIosIcon className="arrowIcon" />
+                    {x.category}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
