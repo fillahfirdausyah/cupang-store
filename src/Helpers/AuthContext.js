7e88cc07bb4efbe37bc16c8fe662b19bf1d6309e
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-27 11:21:24 +0700

    store token

--- src/Helpers/AuthContext.js
+++ src/Helpers/AuthContext.js
@@ -10,7 +10,17 @@ export function AuthProvider({ children }) {
   const [currentToken, setCurrentToken] = useState("");
   const [isAuth, setIsAuth] = useState(false);
 
+  useEffect(() => {
+    let theToken = localStorage.getItem("token");
+    if (theToken == null) {
+      setCurrentToken("");
+    } else {
+      setCurrentToken(theToken);
+    }
+  }, []);
+
   function login(token) {
+    localStorage.setItem("token", token);
     setCurrentToken(token);
     setIsAuth(true);
   }
