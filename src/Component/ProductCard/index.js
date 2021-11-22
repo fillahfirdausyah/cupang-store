0fa0b4a00192e1b5d1f39e30f5871a07d797ebed
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-11-22 15:29:33 +0700

    change Product Card style

--- src/Component/ProductCard/index.js
+++ src/Component/ProductCard/index.js
@@ -1,25 +1,26 @@
 import React from "react";
-import ApiBaseURL from "../../Helpers/apiBaseUrl";
+// import ApiBaseURL from "../../Helpers/apiBaseUrl";
+import CheckIcon from "@material-ui/icons/Check";
 
 import "./style.css";
 
 function ProductCard({ img, name, bestSeller }) {
   return (
     <div className="__productCard" data-aos="fade-up" data-aos-duration="700">
-      <div
-        className={
-          bestSeller
-            ? "__productCardBestSeller"
-            : "__productCardBestSeller hidden"
-        }
-      >
-        <span>Best Seller</span>
-      </div>
       <div className="__productCardBody">
-        <img src={`${ApiBaseURL}/images/${img}`} alt="" />
+        <img src={`${img}`} alt="" />
       </div>
-      <div className="__productCardRibbon">
-        <span>{name}</span>
+      <div className="__productCardInfo">
+        <div className="title">
+          <p>{name}</p>
+        </div>
+        <div className="price">
+          <p>Rp 100.000 <span>Rp.150.000</span></p>
+          <div className="available">
+            <CheckIcon  />
+            Tersedia
+          </div>
+        </div>
       </div>
     </div>
   );
